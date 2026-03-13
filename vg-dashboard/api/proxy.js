ae8a145f6b507c3f68070da0c62ed52828fd8adb
lebronsoc <lebronsoc1@gmail.com>
2026-03-13 16:02:37 +0000

    Change export syntax from default to module.exports

--- vg-dashboard/api/proxy.js
+++ vg-dashboard/api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const url = "https://app.findanomaly.ai/api/dashboards/049d18b4-92a4-4129-8ad5-29aeeccbe141/serve-public?token=961b02b12a27439c051e1004a7307b4da5473fe1b5cb721bebb58490cde98c02";
 
   const response = await fetch(url, {
